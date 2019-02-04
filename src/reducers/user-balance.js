f0e5bdd05dfbece573ee85f3cea608643d500982
d.vetrov@splinestudio.ua <d.vetrov@splinestudio.ua>
2019-02-04 10:44:42 +0200

    Remove Math.Round() from balance reducer

--- src/reducers/user-balance.js
+++ src/reducers/user-balance.js
@@ -5,15 +5,14 @@ export const BALANCE_CHANGE = 'user-balance/BALANCE_CHANGE';
 export default (state = false, action) => {
 	switch (action.type) {
 		case BALANCE_CHANGE:
-			// console.log('BALANCE_CHANGE',action)
-			return Math.round(action.balance);
+			return Number(action.balance);
 		default:
 			return state;
 	}
 }
 
 export const setBalance = (balance) => {
-	console.log("balance", balance)
+	balance = Number(balance).toFixed(2)
 	AsyncStorage.setItem("balance", String(balance));
 	return { type: BALANCE_CHANGE, balance }
 }
