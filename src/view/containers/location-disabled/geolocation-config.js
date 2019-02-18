6434b7ac82c61a5f662dadd0cf49feca0b2af558
d.vetrov@splinestudio.ua <d.vetrov@splinestudio.ua>
2019-02-18 17:56:14 +0200

    Fix geolocation module

--- src/view/containers/location-disabled/geolocation-config.js
+++ src/view/containers/location-disabled/geolocation-config.js
@@ -29,17 +29,26 @@ export default function geo_config() {
         }
         case 'ios': {
             config = {
-                desiredAccuracy: 0,
-                distanceFilter: 1,
-                desiredOdometerAccuracy: 10,
+                // Geolocation Config
+                desiredAccuracy: BackgroundGeolocationModule.DESIRED_ACCURACY_HIGH,
+                distanceFilter: 10,
                 // Activity Recognition
-                stopTimeout: 5,
+                stopTimeout: 1,
                 // Application config
                 debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
                 logLevel: BackgroundGeolocationModule.LOG_LEVEL_VERBOSE,
                 stopOnTerminate: false,   // <-- Allow the background-service to continue tracking when user closes the app.
-                startOnBoot: false,        // <-- Auto start tracking when device is powered-up.
-                stationaryRadius: 1,
+                startOnBoot: true,        // <-- Auto start tracking when device is powered-up.
+                // HTTP / SQLite config
+                url: 'http://yourserver.com/locations',
+                batchSync: false,       // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+                autoSync: true,         // <-- [Default: true] Set true to sync each location to server as it arrives.
+                headers: {              // <-- Optional HTTP headers
+                    "X-FOO": "bar"
+                },
+                params: {               // <-- Optional HTTP params
+                    "auth_token": "maybe_your_server_authenticates_via_token_YES?"
+                }
             }
             break;
         }
