52606a442141476c6f20c491e5b217af80f468a2
Alexander Bilyk <a.bilyk@splinestudio.ua>
2019-04-24 15:33:43 +0300

    tradelist,tradeprice,tradestatus,trcinform,market

--- src/view/containers/trade-list/trade-list.js
+++ src/view/containers/trade-list/trade-list.js
@@ -2,9 +2,9 @@ import React from "react";
 import { View, Text } from "react-native";
 //constants
 import styles from "./styles";
-import PickedLanguage from "./../../../locales/language-picker";
 //services
 import { formatNumber } from "./../../../services/format-number";
+import I18n from "@locales/I18n";
 
 class TradeList extends React.Component {
   render = () => {
@@ -13,51 +13,51 @@ class TradeList extends React.Component {
         {this.props.list.map((item, i) => {
           return (
             <View style={[styles.item, !item.amount && styles.hide]} key={i}>
-                <View style={styles.one_product}>
-                  <View style={styles.data}>
-                    <Text numberOfLines={2} style={styles.text}>{item.product__name}</Text>
-                    {item.amount >= 1 &&
-                      item.approve && (
-                        <Text
-                          style={[styles.text, styles.small]}
-                        >{`${formatNumber(item.product__price)} x ${item.amount} =`}</Text>
-                      )}
-                  </View>
-                  <View
-                    style={[
-                      styles.data,
-                      !item.approve && styles.error,
-                      styles.align
-                    ]}
-                  >
-                    <Text
-                      style={[
-                        styles.text,
-                        item.approve && item.amount > 1 && styles.small,
-                        !item.approve && styles.product__price
-                      ]}
-                    >
-                      {item.amount > 1 && !item.approve
-                        ? `${formatNumber(item.product__price)} x ${
-                        item.amount
-                        } = ${formatNumber(item.product__price * item.amount)}`
-                        : formatNumber(item.product__price)}
-                    </Text>
-                    {item.amount > 1 &&
-                      item.approve && (
-                        <Text style={styles.text}>
-                          {formatNumber(item.product__price * item.amount)}
-                        </Text>
-                      )}
-                  </View>
-                  {(!item.approve || !item.amount )&& (
-                    <View style={[styles.data, styles.align]}>
-                      <Text style={[styles.text, styles.message]}>
-                        {PickedLanguage.TRADE.CANCEL}
-                      </Text>
-                    </View>
+              <View style={styles.one_product}>
+                <View style={styles.data}>
+                  <Text numberOfLines={2} style={styles.text}>
+                    {item.product__name}
+                  </Text>
+                  {item.amount >= 1 && item.approve && (
+                    <Text style={[styles.text, styles.small]}>{`${formatNumber(
+                      item.product__price
+                    )} x ${item.amount} =`}</Text>
                   )}
                 </View>
+                <View
+                  style={[
+                    styles.data,
+                    !item.approve && styles.error,
+                    styles.align
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.text,
+                      item.approve && item.amount > 1 && styles.small,
+                      !item.approve && styles.product__price
+                    ]}
+                  >
+                    {item.amount > 1 && !item.approve
+                      ? `${formatNumber(item.product__price)} x ${
+                          item.amount
+                        } = ${formatNumber(item.product__price * item.amount)}`
+                      : formatNumber(item.product__price)}
+                  </Text>
+                  {item.amount > 1 && item.approve && (
+                    <Text style={styles.text}>
+                      {formatNumber(item.product__price * item.amount)}
+                    </Text>
+                  )}
+                </View>
+                {(!item.approve || !item.amount) && (
+                  <View style={[styles.data, styles.align]}>
+                    <Text style={[styles.text, styles.message]}>
+                      {I18n.t("TRADE.CANCEL")}
+                    </Text>
+                  </View>
+                )}
+              </View>
             </View>
           );
         })}
--- src/view/containers/trade-title/trade-title.js
+++ src/view/containers/trade-title/trade-title.js
@@ -2,16 +2,18 @@ import React from "react";
 import { View, Text } from "react-native";
 //constants
 import styles from "./styles";
-import PickedLanguage from "./../../../locales/language-picker";
 //services
 import { formatDate } from "./../../../services/format-date";
+import I18n from "@locales/I18n";
 
 class TradeTitle extends React.Component {
   render = () => {
     return (
       <View style={styles.container}>
         <View style={styles.data}>
-          <Text style={[styles.text, styles.title]}>{PickedLanguage.TRADE.DATE}</Text>
+          <Text style={[styles.text, styles.title]}>
+            {I18n.t("TRADE.DATE")}
+          </Text>
           <Text style={[styles.text, styles.value]}>
             {formatDate(this.props.date)}
           </Text>
