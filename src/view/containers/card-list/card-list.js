f6cf9ac25e9f2ce31f173e0ef4ce38edef9f0a0f
Alexander Bilyk <a.bilyk@splinestudio.ua>
2019-04-24 16:07:28 +0300

    dateabuse, barcode, card(post-list,list)

--- src/view/containers/card-list/card-list.js
+++ src/view/containers/card-list/card-list.js
@@ -3,10 +3,9 @@ import { View, Text, FlatList } from "react-native";
 //containers
 import Card from "../card/card";
 import OpenedCard from "../opened-card/opened-card";
-import LongCard from "../card-long/card-long"
+import LongCard from "../card-long/card-long";
 import FooterNavigation from "../../containers/footer-navigator/footer-navigator";
 //constants
-import PickedLanguage from "../../../locales/language-picker";
 import styles from "./styles";
 //redux
 import { connect } from "react-redux";
@@ -14,16 +13,18 @@ import { bindActionCreators } from "redux";
 import { setActiveCard } from "../../../reducers/set-active-card";
 import { selectMission } from "../../../reducers/selected-mission";
 import { setDashboardState } from "../../../reducers/dashboard-state";
-import { orderBy } from 'lodash';
+import { orderBy } from "lodash";
+import I18n from "@locales/I18n";
 
 class CardList extends React.Component {
-
-  _renderItem = item => (
-    item.item.type != "instagram_connect" && item.item.type != "facebook_connect" ?
-      <Card item={item.item} onPressItem={this._showSelectedCard} /> :
+  _renderItem = item =>
+    item.item.type != "instagram_connect" &&
+    item.item.type != "facebook_connect" ? (
+      <Card item={item.item} onPressItem={this._showSelectedCard} />
+    ) : (
       <LongCard item={item.item} onPressItem={this.props.connectSocial} />
-  );
-  _submissionOrder = (mission) => {
+    );
+  _submissionOrder = mission => {
     let flag = true;
     mission.subMissions.forEach(sub_mission => {
       if (sub_mission.type === 2) {
@@ -31,24 +32,24 @@ class CardList extends React.Component {
       }
     });
     let insta_sub_mission = {
-      desc: PickedLanguage.MISSION.SUBMISSION_3,
+      desc: I18n.t("MISSION.SUBMISSION_3"),
       id: 1,
       name: "insta submission",
       type: 2,
-      flatlistData: [],
-    }
-    mission.subMissions.length && (mission.subMissions = orderBy(mission.subMissions, ['type'], ['desc']))
-    if (flag) 
-      mission.subMissions.push(insta_sub_mission)
-    return mission
-  }
+      flatlistData: []
+    };
+    mission.subMissions.length &&
+      (mission.subMissions = orderBy(mission.subMissions, ["type"], ["desc"]));
+    if (flag) mission.subMissions.push(insta_sub_mission);
+    return mission;
+  };
   _showSelectedCard = selectedCard => {
     this.props.setDashboardState(2);
     this.props.setActiveCard(true);
     this.props.selectMission(this._submissionOrder(selectedCard));
   };
   keyExtractor = item => {
-    return item.id + item.type + ""
+    return item.id + item.type + "";
   };
   render() {
     return (
@@ -56,8 +57,8 @@ class CardList extends React.Component {
         {!this.props.activeCard ? (
           <View style={styles.list_view}>
             {this.props.missions.length > 0 ? (
-              this.props.missions.length - this.props.socialCount == 1 ?
-                < View style={styles.list_view}>
+              this.props.missions.length - this.props.socialCount == 1 ? (
+                <View style={styles.list_view}>
                   <FlatList
                     key={"solo_mission"}
                     contentContainerStyle={styles.contentContainerStyle}
@@ -75,15 +76,17 @@ class CardList extends React.Component {
                     onScrollBeginDrag={() => this.props.onScrollBeginDrag()}
                   />
                 </View>
-                :
-                < View style={styles.list_view}>
+              ) : (
+                <View style={styles.list_view}>
                   <FlatList
                     key={"multi_mission"}
                     contentContainerStyle={styles.contentContainerStyle}
                     style={styles.list_view}
                     horizontal={false}
                     numColumns={2}
-                    columnWrapperStyle={{ flexWrap: this.props.socialCount === 1 ? 'nowrap' : 'wrap' }}
+                    columnWrapperStyle={{
+                      flexWrap: this.props.socialCount === 1 ? "nowrap" : "wrap"
+                    }}
                     keyExtractor={this.keyExtractor}
                     data={this.props.missions}
                     renderItem={this._renderItem}
@@ -95,16 +98,16 @@ class CardList extends React.Component {
                     onScrollBeginDrag={() => this.props.onScrollBeginDrag()}
                   />
                 </View>
+              )
             ) : (
-                <View style={styles.no_tasks}>
-                  <Text style={styles.no_tasks_text}>{PickedLanguage.NO_TASKS}</Text>
-                </View>
-              )}
+              <View style={styles.no_tasks}>
+                <Text style={styles.no_tasks_text}>{I18n.t("NO_TASKS")}</Text>
+              </View>
+            )}
           </View>
         ) : (
-            <OpenedCard style={!this.props.activeCard ? styles.disabled : null} />
-          )
-        }
+          <OpenedCard style={!this.props.activeCard ? styles.disabled : null} />
+        )}
       </View>
     );
   }
--- src/view/containers/card-posts-list/card-posts-list.js
+++ src/view/containers/card-posts-list/card-posts-list.js
@@ -1,61 +1,48 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
 //containers
-import Card from "../card/card";
-import PostCard from "../card-post/card-post"
+import PostCard from "../card-post/card-post";
 //constants
-import PickedLanguage from "../../../locales/language-picker";
 import styles from "./styles";
 //redux
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { setActiveCard } from "../../../reducers/set-active-card";
-import { selectMission } from "../../../reducers/selected-mission";
-import { setDashboardState } from "../../../reducers/dashboard-state";
-import { orderBy } from 'lodash';
+import I18n from "@locales/I18n";
 
 class CardPostsList extends React.Component {
-
-    _renderItem = item => (
-        <PostCard item={item.item} />
+  _renderItem = item => <PostCard item={item.item} />;
+  keyExtractor = item => item.id;
+  render() {
+    return (
+      <View style={styles.list_view}>
+        {this.props.posts.length > 0 ? (
+          <FlatList
+            contentContainerStyle={styles.contentContainerStyle}
+            style={styles.list_view}
+            horizontal={false}
+            numColumns={1}
+            removeClippedSubviews={true}
+            keyExtractor={this.keyExtractor}
+            data={this.props.posts}
+            renderItem={this._renderItem}
+          />
+        ) : (
+          <View style={styles.no_tasks}>
+            <Text style={styles.no_tasks_text}>{I18n.t("NO_POSTS")}</Text>
+          </View>
+        )}
+      </View>
     );
-    keyExtractor = item => item.id;
-    render() {
-        return (
-            <View style={styles.list_view}>
-                {this.props.posts.length > 0 ? (
-                    <FlatList
-                        contentContainerStyle={styles.contentContainerStyle}
-                        style={styles.list_view}
-                        horizontal={false}
-                        numColumns={1}
-                        removeClippedSubviews={true}
-                        keyExtractor={this.keyExtractor}
-                        data={this.props.posts}
-                        renderItem={this._renderItem}
-                    />
-                ) : (
-                        <View style={styles.no_tasks}>
-                            <Text style={styles.no_tasks_text}>{PickedLanguage.NO_POSTS}</Text>
-                        </View>
-                    )}
-            </View>
-        );
-    }
+  }
 }
 
 const mapStateToProps = state => ({
-    userColor: state.userColor,
+  userColor: state.userColor
 });
 
-const mapDispatchToProps = dispatch =>
-    bindActionCreators(
-        {
-        },
-        dispatch
-    );
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(CardPostsList);
