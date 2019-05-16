e52a05480f0f0db4a249d5092fa4f3554467ba3e
Alexandr Serebrianyi <a.serebrianyi@splinestudio.ua>
2019-05-16 10:43:22 +0300

    lint post-to-social

--- src/services/post-to-social.js
+++ src/services/post-to-social.js
@@ -1,55 +1,55 @@
-import { Linking, Platform, PermissionsAndroid } from "react-native";
-import RNInstagramStoryShare from "../native_modules/react-native-instagram-story-share/src";
-import RNFetchBlob from "rn-fetch-blob";
-import RNFS from "react-native-fs";
-import Share from "react-native-share";
-import I18n from "@locales/I18n";
+import { Linking, Platform, PermissionsAndroid } from 'react-native';
+import RNInstagramStoryShare from '../native_modules/react-native-instagram-story-share';
+import RNFetchBlob from 'rn-fetch-blob';
+import RNFS from 'react-native-fs';
+import Share from 'react-native-share';
+import I18n from '@locales/I18n';
 
 confirmFuction = () => {
   //this func will be overrided for iOS for callBack
-  // console.log("confirmFuction not overrided");
+  console.log('confirmFuction not overrided');
 };
 callCallback = callback => {
-  // console.log("instagram callback: ", callback);
-  if (Platform.OS != "ios") {
+  console.log('instagram callback: ', callback);
+  if (Platform.OS != 'ios') {
     let filePath;
-    if (callback === "exception_video" || callback === "success_video") {
-      filePath = "/storage/emulated/0/DCIM/epc_game_video.mp4";
+    if (callback === 'exception_video' || callback === 'success_video') {
+      filePath = '/storage/emulated/0/DCIM/epc_game_video.mp4';
     } else {
-      filePath = "/storage/emulated/0/DCIM/epc_game_img.jpg";
+      filePath = '/storage/emulated/0/DCIM/epc_game_img.jpg';
     }
     setTimeout(() => {
       RNFS.exists(filePath)
         .then(res => {
-          // console.log(res);
+          console.log(res);
           if (res) {
             RNFS.unlink(filePath)
               .then(() => {
-                // console.log("unlink success");
+                console.log('unlink success');
                 this.confirmFuction();
               })
               .catch(err => {
-                // console.log("unlink error", err);
+                console.log('unlink error', err);
               });
           }
         })
         .catch(err => {
-          // console.log("filePath exists error", err);
+          console.log('filePath exists error', err);
         });
     }, 1000);
   }
-  if (callback === "no_instagram") {
-    if (Platform.OS === "ios") {
-      Linking.openURL("itms-apps://itunes.apple.com/app/instagram/id389801252");
+  if (callback === 'no_instagram') {
+    if (Platform.OS === 'ios') {
+      Linking.openURL('itms-apps://itunes.apple.com/app/instagram/id389801252');
     } else {
-      Linking.openURL("market://details?id=com.instagram.android");
+      Linking.openURL('market://details?id=com.instagram.android');
     }
   }
 };
 
 // export function postToSocialStory(postData, deepLink, confirmFuction) {
 //   //deprecated
-//   if (Platform.OS === "ios") {
+//   if (Platform.OS === 'ios') {
 //     RNInstagramStoryShare.share(
 //       {
 //         backgroundImage: postData.base64,
@@ -60,10 +60,10 @@ callCallback = callback => {
 //     );
 //     confirmFuction();
 //   } else {
-//     let image_data = postData.base64.split("data:image/jpg;base64,")[1];
+//     let image_data = postData.base64.split('data:image/jpg;base64,')[1];
 //     const dirs = RNFetchBlob.fs.dirs;
-//     const file_path = dirs.DCIMDir + "/epc_game_img.jpg";
-//     RNFS.writeFile(file_path, image_data, "base64")
+//     const file_path = dirs.DCIMDir + '/epc_game_img.jpg';
+//     RNFS.writeFile(file_path, image_data, 'base64')
 //       .then(() => {
 //         RNInstagramStoryShare.share(
 //           {
@@ -75,24 +75,24 @@ callCallback = callback => {
 //         );
 //       })
 //       .catch(err => {
-//         console.log("writeFile error", err);
+//         console.log('writeFile error', err);
 //       });
 //   }
 // }
 export function postToSocial(postData, deepLink, confirmFuction, video_status) {
-  let base64Prefix = "data:image/jpg;base64,";
+  let base64Prefix = 'data:image/jpg;base64,';
   const dirs = RNFetchBlob.fs.dirs;
   let file_path;
   let type;
-  if (Platform.OS === "ios") {
+  if (Platform.OS === 'ios') {
     if (video_status) {
-      base64Prefix = "data:video/mp4;base64,";
-      postData.base64 = "";
+      base64Prefix = 'data:video/mp4;base64,';
+      postData.base64 = '';
       RNFetchBlob.config({
         fileCache: true,
-        appendExt: "mp4"
+        appendExt: 'mp4'
       })
-        .fetch("GET", postData.video) //postData
+        .fetch('GET', postData.video) //postData
         .then(res => {
           let shareImageBase64 = {
             url: res.path()
@@ -106,15 +106,15 @@ export function postToSocial(postData, deepLink, confirmFuction, video_status) {
                     if (result) {
                       RNFS.unlink(res.path())
                         .then(() => {
-                          // console.log("unlink success");
+                          console.log('unlink success');
                         })
                         .catch(err => {
-                          // console.log("unlink error", err);
+                          console.log('unlink error', err);
                         });
                     }
                   })
                   .catch(err => {
-                    // console.log("filePath exists error", err);
+                    console.log('filePath exists error', err);
                   });
               },
               error => {}
@@ -122,14 +122,13 @@ export function postToSocial(postData, deepLink, confirmFuction, video_status) {
           }, 2000);
         });
     } else {
-      base64Prefix = "data:image/jpg;base64,";
+      base64Prefix = 'data:image/jpg;base64,';
       let shareImageBase64 = {
         url:
           postData.base64 && postData.base64.includes(base64Prefix)
             ? postData.base64
             : base64Prefix + postData.base64 //check for base64 prefix
       };
-      // console.log(shareImageBase64);
       setTimeout(() => {
         Share.open(shareImageBase64).then(
           result => {
@@ -142,9 +141,9 @@ export function postToSocial(postData, deepLink, confirmFuction, video_status) {
   } else {
     let post_data;
     if (video_status) {
-      type = "mp4";
+      type = 'mp4';
       post_data = postData.video; //postData
-      file_path = dirs.DCIMDir + "/epc_game_video.mp4";
+      file_path = dirs.DCIMDir + '/epc_game_video.mp4';
       requestStoragePermission(
         post_data,
         file_path,
@@ -154,13 +153,13 @@ export function postToSocial(postData, deepLink, confirmFuction, video_status) {
         video_status
       );
     } else {
-      type = "base64";
+      type = 'base64';
       if (postData.base64 && postData.base64.includes(base64Prefix)) {
-        post_data = postData.base64.split("data:image/jpg;base64,")[1];
+        post_data = postData.base64.split('data:image/jpg;base64,')[1];
       } else {
         post_data = postData.base64;
       }
-      file_path = dirs.DCIMDir + "/epc_game_img.jpg";
+      file_path = dirs.DCIMDir + '/epc_game_img.jpg';
       RNFS.writeFile(file_path, post_data, type)
         .then(() => {
           RNInstagramStoryShare.shareToFeed(
@@ -175,7 +174,7 @@ export function postToSocial(postData, deepLink, confirmFuction, video_status) {
           this.confirmFuction = confirmFuction; //override this.confirmFuction to call confirmFunction in callback
         })
         .catch(err => {
-          // console.log("writeFile error", err);
+          console.log('writeFile error', err);
           requestStoragePermission(
             post_data,
             file_path,
@@ -200,27 +199,27 @@ async function requestStoragePermission(
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
       {
-        title: I18n.t("PERMISSON.STORAGE_PERMISSON_TITLE"),
-        message: I18n.t("PERMISSON.STORAGE_PERMISSON_MESSAGE"),
-        buttonNegative: I18n.t("PROFILE_PAGE.DECLINE"),
-        buttonPositive: I18n.t("OK").toUpperCase()
+        title: I18n.t('PERMISSON.STORAGE_PERMISSON_TITLE'),
+        message: I18n.t('PERMISSON.STORAGE_PERMISSON_MESSAGE'),
+        buttonNegative: I18n.t('PROFILE_PAGE.DECLINE'),
+        buttonPositive: I18n.t('OK').toUpperCase()
       }
     );
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      // console.log("You can use the WRITE_EXTERNAL_STORAGE");
+      console.log('You can use the WRITE_EXTERNAL_STORAGE');
       if (video_status) {
         RNFetchBlob.config({
           addAndroidDownloads: {
             useDownloadManager: true,
             notification: false,
             path: file_path,
-            mime: "text/plain",
-            description: "File downloaded by download manager."
+            mime: 'text/plain',
+            description: 'File downloaded by download manager.'
           }
         })
-          .fetch("GET", post_data)
+          .fetch('GET', post_data)
           .then(resp => {
-            // console.log(resp);
+            console.log(resp);
             RNInstagramStoryShare.shareToFeed(
               {
                 backgroundImage: file_path,
@@ -233,7 +232,7 @@ async function requestStoragePermission(
             this.confirmFuction = confirmFuction; //override this.confirmFuction to call confirmFunction in callback
           })
           .catch(err => {
-            // console.log("writeFile error", err);
+            console.log('writeFile error', err);
           });
       } else {
         RNFS.writeFile(file_path, post_data, type)
@@ -250,11 +249,11 @@ async function requestStoragePermission(
             this.confirmFuction = confirmFuction; //override this.confirmFuction to call confirmFunction in callback
           })
           .catch(err => {
-            // console.log("writeFile error", err);
+            console.log('writeFile error', err);
           });
       }
     } else {
-      // console.log("WRITE_EXTERNAL_STORAGE permission denied");
+      console.log('WRITE_EXTERNAL_STORAGE permission denied');
     }
   } catch (err) {
     console.warn(err);
