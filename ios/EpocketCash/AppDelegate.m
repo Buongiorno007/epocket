d64bf6d6d631aa623ee88e6633700287d8b87387
Alexander Bilyk <a.bilyk@splinestudio.ua>
2019-05-20 14:04:38 +0300

    update package modules

--- ios/EpocketCash/AppDelegate.m
+++ ios/EpocketCash/AppDelegate.m
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -13,39 +6,36 @@
 
 @import GoogleMaps;
 @import Firebase;
-
-//@import YMMYandexMetrica;
 @import YandexMobileMetrica;
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"EpocketCash"
-                                            initialProperties:nil];
-  [Fabric with:@[[Crashlytics class]]];
-  [GMSServices provideAPIKey:@"AIzaSyBzxCpV9ME-F2W8rz-ZkOsZrYfgmpPdhEw"];
-  [FIRApp configure];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+	- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+	{
+		[FIRApp configure];
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  YMMYandexMetricaConfiguration *configuration = [[YMMYandexMetricaConfiguration alloc] initWithApiKey:@"e455a5ed-4f92-4071-8b46-5822489758d5"];
-  [YMMYandexMetrica activateWithConfiguration:configuration];
-  return YES;
-}
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
-}
+		RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+		RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+														moduleName:@"EpocketCash"
+													initialProperties:nil];
+
+		[GMSServices provideAPIKey:@"AIzaSyBzxCpV9ME-F2W8rz-ZkOsZrYfgmpPdhEw"];
+		
+		rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+		self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+		UIViewController *rootViewController = [UIViewController new];
+		rootViewController.view = rootView;
+		self.window.rootViewController = rootViewController;
+		[self.window makeKeyAndVisible];
+		YMMYandexMetricaConfiguration *configuration = [[YMMYandexMetricaConfiguration alloc] initWithApiKey:@"e455a5ed-4f92-4071-8b46-5822489758d5"];
+		[YMMYandexMetrica activateWithConfiguration:configuration];
+		return YES;
+	}
+
+	- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+	{
+		return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+	}
   
 @end
