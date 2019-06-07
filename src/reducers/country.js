11ac9d972796a82e12058e97e0c131b6273256f7
Alexander Bilyk <a.bilyk@splinestudio.ua>
2019-06-07 09:48:23 +0300

    refactoring country list reducer

--- src/reducers/country.js
+++ src/reducers/country.js
@@ -6,28 +6,26 @@ import { updateServerRequest } from '@reducers/serverRequest'
 
 const initialState = new COUNTRY()
 
-const RESULT = '[country]/RESULT'
+const RESULT = '[country] RESULT'
 
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case RESULT:
 			return action.payload
 		default:
-			return initialState
+			return state
 	}
 }
 
 export const list = () => async (dispatch, getState) => {
 	const { country } = getState()
 	if (country.length === 0) {
-		dispatch(loaderState(true))
 		try {
-			await dispatch(loaderState(false))
 			await dispatch(updateServerRequest(false))
 			const response = await httpGet(urls.echo)
 			await dispatch(result(new COUNTRY(response.body)))
 		} catch (e) {
-			country()
+			list()
 			dispatch(updateServerRequest(true))
 			dispatch(result(new COUNTRY(e)))
 		}
