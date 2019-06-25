529df2faca97346c883a24da6c06881a22234c0a
Alexander <alexanderangular@gmail.com>
2019-06-25 10:15:34 +0300

    create finish success container

---
+++ src/view/containers/refill/success/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { Button } from 'native-base'
+import I18n from '@locales/I18n'
+import route from '@services/route'
+import styles from './styles'
+
+type Props = {
+	phone: string,
+	color: string,
+}
+
+function Success({ phone, color }: Props) {
+	const navigate = () => route.navigate('Main')
+	return (
+		<View style={styles.layout}>
+			<Text style={styles.thanks}>{I18n.t('REFILL.THANKS')}</Text>
+			<Text style={styles.result}>{I18n.t('REFILL.SUCCESS', { phone })}</Text>
+			<Button rounded block style={styles.button} onPress={navigate}>
+				<Text style={[styles.text, { color }]}>{I18n.t('OK')}</Text>
+			</Button>
+		</View>
+	)
+}
+
+const mapStateToProps = (state) => ({
+	color: state.userColor.second_gradient_color,
+	phone: state.bonuses.phone,
+})
+
+export default connect(
+	mapStateToProps,
+	null,
+)(Success)
---
+++ src/view/containers/refill/success/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+	layout: {
+		flexGrow: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingHorizontal: 16,
+	},
+	thanks: {
+		fontSize: 15,
+		fontFamily: 'Rubik-Bold',
+		lineHeight: 18,
+		color: '#fff',
+		marginBottom: 20,
+	},
+	result: {
+		fontSize: 14,
+		lineHeight: 16,
+		fontFamily: 'Rubik-Regular',
+		color: '#fff',
+		marginBottom: 20,
+		textAlign: 'center',
+	},
+	button: {
+		backgroundColor: '#fff',
+		marginTop: 30,
+	},
+	text: {
+		fontSize: 12,
+		lineHeight: 12,
+		fontFamily: 'Rubik-Medium',
+		letterSpacing: 2,
+	},
+})
