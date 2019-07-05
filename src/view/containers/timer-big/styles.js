20bf1852cd0206827fa410d2bf8ae5578aef1320
Alexandr Serebrianyi <a.serebrianyi@splinestudio.ua>
2019-07-05 09:13:43 +0300

    removed not used containers

--- src/view/containers/timer-big/styles.js
+++
@@ -1,6 +0,0 @@
-import { StyleSheet, Dimensions } from 'react-native'
-
-const { width, height } = Dimensions.get('window')
-import { colors } from '../../../constants/colors'
-
-export default StyleSheet.create({})
