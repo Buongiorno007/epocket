90022dd627cfe699c2be8b15818af4df03ec6c5d
Alexandr Serebrianyi <alexandrserebrianyi@MacBook-Pro-Alexandr.local>
2019-08-01 13:06:10 +0300

    mapSpend start

--- src/view/components/map/map-spend/index.js
+++ src/view/components/map/map-spend/index.js
@@ -1,8 +1,63 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ScrollView, Platform, Image } from 'react-native'
+import { connect } from 'react-redux'
+import ClusteredMapView from '../../../../native_modules/react-native-maps-super-cluster'
+import { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 
 import styles from './styles'
 
-export default function MapSpend(props) {
-	return <View style={styles.container}></View>
+function MapSpend({ lat, lng, mapPoints }) {
+	const region = {
+		latitude: lat,
+		longitude: lng,
+		latitudeDelta: 0.006,
+		longitudeDelta: 0.006,
+	}
+	console.log(mapPoints, 'MAPPOINTS CASHOUTS')
+	const renderMarker = (data) => {
+		return (
+			<View
+				key={`${data.id}`}
+				style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: 'linghtblue' }}
+			/>
+		)
+	}
+	return (
+		<View style={styles.container}>
+			<ScrollView style={styles.scroll}>
+				<View style={{ flex: 1, height: 150 }}>
+					<ClusteredMapView
+						style={{ flex: 1 }}
+						data={mapPoints.cashouts}
+						initialRegion={region}
+						provider={Platform.OS === 'ios' ? PROVIDER_GOOGLE : null}
+						renderMarker={renderMarker}
+						animateClusters={false}
+						showsCompass={false}
+						edgePadding={{ top: 50, left: 50, bottom: 50, right: 50 }}
+					>
+						<Marker
+							coordinate={{
+								latitude: lat,
+								longitude: lng,
+							}}
+						>
+							<Image style={{ width: 40, height: 40 }} source={require('@assets/img/smile.png')} />
+						</Marker>
+					</ClusteredMapView>
+				</View>
+			</ScrollView>
+		</View>
+	)
 }
+
+const mapStateToProps = (state) => {
+	return {
+		mapPoints: state.mapPoints,
+		lat: state.location.coordinate.lat,
+		lng: state.location.coordinate.lng,
+		profileState: state.profileState,
+	}
+}
+
+export default connect(mapStateToProps)(MapSpend)
--- src/view/components/map/map-spend/styles.js
+++ src/view/components/map/map-spend/styles.js
@@ -3,7 +3,11 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
 	container: {
 		flex: 1,
-		marginBottom: 60, //paddingBottom: 60,
+		marginBottom: 60,
 		backgroundColor: 'lightblue',
+	},
+	scroll: {
+		flex: 1,
+		paddingHorizontal: 16,
 	},
 })
