02746312f7da246717d2c4f2caadad6673047111
Alexandr Serebrianyi <alexandrserebrianyi@MacBook-Pro-Alexandr.local>
2019-08-30 10:14:32 +0300

    added main basket

--- src/view/components/basket/index.js
+++ src/view/components/basket/index.js
@@ -1,20 +1,34 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import LinearGradient from 'react-native-linear-gradient'
 import MapHeaderWhite from '@containers/map/map-header-white'
 import I18n from '@locales/I18n'
 //styles
 import styles from './styles'
+import BasketItem from '@containers/basket-item'
 
-function BasketComponent({ balance, profileState }) {
+function BasketComponent({ balance, profileState, basket }) {
 	const colors = ['#F55890', '#FF9950']
 	const start = { x: 0.0, y: 0.0 }
 	const end = { x: 0.0, y: 1.0 }
+
+	const renderItem = ({ item }) => <BasketItem item={item} />
+	const keyExtractor = (item) => `${item.point_id}`
+
 	return (
 		<View style={styles.container}>
 			<LinearGradient start={start} end={end} colors={colors} style={styles.container}>
 				<MapHeaderWhite title={`${I18n.t('CASH.TITLE')} ${balance} ${profileState.currency}`} />
+				<View style={styles.textView}>
+					<Text style={styles.text}>{'Список магазинов, в которых содержатся ваши предзаказы'}</Text>
+				</View>
+				<FlatList
+					style={styles.scroll}
+					data={basket.data}
+					renderItem={renderItem}
+					keyExtractor={keyExtractor}
+				/>
 			</LinearGradient>
 		</View>
 	)
@@ -23,6 +37,7 @@ function BasketComponent({ balance, profileState }) {
 const mapStateToProps = (state) => {
 	return {
 		balance: state.balance,
+		basket: state.basket,
 		profileState: state.profileState,
 	}
 }
--- src/view/components/basket/styles.js
+++ src/view/components/basket/styles.js
@@ -7,4 +7,25 @@ export default StyleSheet.create({
 	container: {
 		flex: 1,
 	},
+	textView: {
+		width,
+		height: width * 0.6 - 90,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingHorizontal: 16,
+	},
+	text: {
+		fontFamily: 'Rubik-Medium',
+		fontSize: 18,
+		color: '#fff',
+		textAlign: 'center',
+	},
+	scroll: {
+		flex: 1,
+		backgroundColor: '#E5EDF7',
+		borderTopLeftRadius: 24,
+		borderTopRightRadius: 24,
+		paddingTop: 8,
+		paddingHorizontal: 16,
+	},
 })
