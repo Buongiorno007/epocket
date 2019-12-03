394970c7f762b5efef13fe849c0b7ae816651589
Hilenko Igor <i.khylenko@splinestudio.ua>
2019-12-03 18:32:43 +0200

    markerOutline

--- src/view/components/map/map-places/styles.js
+++ src/view/components/map/map-places/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Dimensions } from 'react-native'
+import { colors } from '@constants/colors'
 const { width } = Dimensions.get('window')
 
 export default StyleSheet.create({
@@ -18,4 +19,44 @@ export default StyleSheet.create({
 		fontFamily: 'Rubik-Medium',
 		fontSize: 16,
 	},
+	markerOutline: {
+		width: 48,
+		height: 48,
+		borderRadius: 24,
+		borderWidth: 2,
+		borderColor: colors.black111,
+		backgroundColor: colors.transparent,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	infobox: {
+		position: 'absolute', 
+		left: 16,
+		right: 16,
+		bottom: 60, 
+		backgroundColor: 'white', 
+		borderRadius: 10, 
+		padding: 10
+	},
+	infobox_image: { 
+		width: 48, 
+		height: 48, 
+		borderRadius: 24, 
+	},
+	infobox_image_outline: {
+		width: 56,
+		height: 56,
+		backgroundColor: colors.transparent,
+		borderWidth: 2,
+		borderColor: colors.blood_red,
+		borderRadius: 28,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 16 
+	},
+	infobox_title: {
+		fontFamily: 'Rubik-Medium',
+		fontSize: 14,
+		color: colors.black111
+	}
 })
